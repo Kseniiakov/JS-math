e1b36a988afe3f160678ada9a97772dd75ae02c1
Kseniia Kovalenko <kseniiakovalenko.94@gmail.com>
2022-01-25 17:01:26 +0200

    Add all S and V

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+// const a = prompt ('Введите основание треугольника');
+// const na = Number (a)
+// const h = prompt ('Введите высоту треугольника');
+// const nh = Number (h)
+// alert(1/2*(na*nh));
+
+// const a = prompt ('Введите сторону трапеции');
+// const na = Number (a)
+// const b = prompt ('Введите основание трапеции');
+// const nb = Number (b)
+// const h = prompt ('Введите высоту трапеции');
+// const nh = Number (h)
+// alert(((na*nb)/2)*nh);
+
+// const r = prompt ('Введите радиус круга');
+// const nr = Number (r);
+// const S = Math.PI*nr**2;
+// alert(S);
+
+// const a = prompt ('Введите основание параллелограммa');
+// const na = Number (a)
+// const h = prompt ('Введите высоту параллелограммa');
+// const nh = Number (h)
+// const S = na*nh
+// alert(S);
+
+// const r = prompt ('Введите радиус цилиндра');
+// const nr = Number (r);
+// const h = prompt ('Введите высоту цилиндра');
+// const nh = Number (h)
+// const V = Math.PI*nh*nr**2;
+// alert(V);
+
+const r = prompt ('Введите радиус сферы');
+const nr = Number (r);
+const V = 4/3*Math.PI*nr**3;
+alert(V);
